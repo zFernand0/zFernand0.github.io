7ee4710639721a009aeb71c24de0d69826b772ad
Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>
2024-11-02 19:04:14 -0400

    chore: add navbar
    Signed-off-by: Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>

---
+++ docs/js/main.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("show");
+}
+function hideMenu() {
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.remove("show");
+}
